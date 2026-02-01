d18f3431b571552ca8457824f1ee1bf09a2137fe
Maksym Huk <maksym.h1009@gmail.com>
2026-02-01 17:40:46 +0200

    result

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,21 @@
  *
  * @return {number}
  */
+
+const amountPerDay = 40;
+const discountFor3To6Days = 20;
+const discountFor7OrMoreDays = 50;
+
 function calculateRentalCost(days) {
-  // write code here
+  let totalCost = days * amountPerDay;
+
+  if (days >= 7) {
+    totalCost -= discountFor7OrMoreDays;
+  } else if (days >= 3) {
+    totalCost -= discountFor3To6Days;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
